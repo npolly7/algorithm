dc921cddd911cdb31e9c500317d2c4a03a234010
npolly7 <npolly7@naver.com>
2019-04-09 22:38:08 +0900

    first commit

---
+++ Algorithm/src/Solution.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+//사랑의 카운슬러 (지렁이 문제)
+public class Solution {
+	static Vec[] vec;
+	static boolean[] visited;
+	static int N, r;
+ static long answer;
+
+ public static void main(String args[]) throws Exception {
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  StringBuilder sb = new StringBuilder();
+  StringTokenizer st;
+ 
+  //TestCase 입력받
+  int T = Integer.parseInt(br.readLine());
+ 
+  for (int test_case = 1; test_case <= T; test_case++) {
+	  //지렁이 몇마리
+   N = Integer.parseInt(br.readLine());
+   
+   //이건뭐지??
+   r = N / 2;
+   
+   //이거 왜만들어??
+   vec = new Vec[N];
+   
+   //이거는 방문한 노드라는 거고...
+   visited = new boolean[N];
+ 
+   //N만큼 for문을 돌면서.. 
+   for (int i = 0; i < N; i++) {
+    st = new StringTokenizer(br.readLine());
+    //vec 
+    vec[i] = new Vec(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+   }
+ 
+   answer = Long.MAX_VALUE;
+   dfs(0, 0);
+ 
+   sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+  }
+  bw.write(sb.toString());
+  bw.close();
+  br.close();
+ }
+ 
+ static void dfs(int depth, int start) {
+  if (depth == r) {
+   long sX = 0, sY = 0;
+ 
+   for (int i = 0; i < N; i++) {
+    sX += vec[i].x * (visited[i] ? 1 : -1);
+    sY += vec[i].y * (visited[i] ? 1 : -1);
+   }
+ 
+   long tmp = sX * sX + sY * sY;
+   answer = answer < tmp ? answer : tmp;
+   return;
+  }
+ 
+  for (int i = start; i < N; i++) {
+   if (!visited[i]) {
+    visited[i] = true;
+    dfs(depth + 1, i + 1);
+    visited[i] = false;
+   }
+  }
+ }
+ 
+ static class Vec {
+  int x, y;
+ 
+  Vec(int x, int y) {
+   this.x = x;
+   this.y = y;
+  }
+ }
+}
